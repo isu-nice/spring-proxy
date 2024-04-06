18f539976c8b7345ac51ea9c9b3890c3c5f6ac13
isu-nice <isu010614@naver.com>
2024-04-06 10:11:12 +0900

    feat: 리포지토리 프록시 구성

---
+++ src/main/java/hello/proxy/config/v1_proxy/OrderRepositoryInterfaceProxy.java
@@ -0,0 +1,27 @@
+package hello.proxy.config.v1_proxy;
+
+import hello.proxy.app.v1.OrderRepositoryV1;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class OrderRepositoryInterfaceProxy implements OrderRepositoryV1 {
+
+    private final OrderRepositoryV1 target; // proxy 에서는 내가 호출할 대상을 보통 target 이라고 함
+    private final LogTrace logTrace;
+
+    @Override
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderRepository.request()");
+            // target 호출
+            target.save(itemId);
+            logTrace.end(status);
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e; // 흐름을 변경하면 안되니까 예외 던져줘야 함
+        }
+    }
+}
