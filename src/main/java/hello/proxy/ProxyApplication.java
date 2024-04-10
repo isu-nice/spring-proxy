9973fa14baf7d30c89308c96908fe6207f909e3e
isu-nice <isu010614@naver.com>
2024-04-10 21:48:02 +0900

    feat: config import

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -2,12 +2,13 @@ package hello.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+@Import(AppV1Config.class)
+@SpringBootApplication(scanBasePackages = "hello.proxy.app") // 해당 경로 하위만 컴포넌트 스캔됨
 public class ProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
-
 }
