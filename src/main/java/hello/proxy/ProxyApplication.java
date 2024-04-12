93f826979b38a74cfd8ba3ed239eabe2db9f073c
isu-nice <isu010614@naver.com>
2024-04-12 16:05:52 +0900

    fix: config import 변경

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
+//@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") // 해당 경로 하위만 컴포넌트 스캔됨
 public class ProxyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProxyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
 }
