5dcdc5a872a897c6066b4c4d905674380b92ddc9
isu-nice <isu010614@naver.com>
2024-04-18 18:11:46 +0900

    test: 클래스, 메서드 메타 정보 확인 테스트

--- src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
+++ src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -1,13 +1,17 @@
 package hello.proxy.jdkdynamic;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
 
 @Slf4j
 public class ReflectionTest {
 
     @Test
-    void reflection() {
+    @DisplayName("기본 호출 로직")
+    void reflection0() {
         Hello target = new Hello();
 
         // 공통 로직1 시작
@@ -21,6 +25,24 @@ public class ReflectionTest {
         String result2 = target.callB(); // 호출하는 메서드가 다름
         log.info("result={}", result2);
         // 공통 로직2 종료
+    }
+
+    @Test
+    @DisplayName("클래스, 메서드 메타 정보 확인 테스트")
+    void reflection1() throws Exception {
+        // 클래스 메타 정보 (내부 클래스는 구분을 위해 $ 표시를 사용함)
+        Class classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+
+        Hello target = new Hello();
+        // callA 메서드 메타 정보
+        Method methodCallA = classHello.getMethod("callA");
+        Object result1 = methodCallA.invoke(target);
+        log.info("result1={}", result1);
+
+        // callB 메서드 메타 정보
+        Method methodCallB = classHello.getMethod("callB");
+        Object result2 = methodCallB.invoke(target);
+        log.info("result2={}", result2);
     }
 
     @Slf4j
