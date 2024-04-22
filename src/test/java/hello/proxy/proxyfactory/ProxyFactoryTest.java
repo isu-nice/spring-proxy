16da7da02cf2093ee0bd30e4686202b78fc009ae
isu-nice <isu010614@naver.com>
2024-04-22 17:15:55 +0900

    test: 구체 클래스 -> CGLIB 사용 테스트

--- src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
+++ src/test/java/hello/proxy/proxyfactory/ProxyFactoryTest.java
@@ -37,4 +37,24 @@ public class ProxyFactoryTest {
         assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
     }
 
+    @Test
+    @DisplayName("구체 클래스만 있으면 CGLIB 사용")
+    void concreteProxy() {
+        // 구체 클래스
+        ConcreteService target = new ConcreteService();
+
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+
+        ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.call();
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
+
 }
