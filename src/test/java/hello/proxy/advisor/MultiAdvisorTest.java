e3050c69cfd500a5fcbf13de97f84169745204f3
isu-nice <isu010614@naver.com>
2024-04-23 18:07:08 +0900

    test: 단일 프록시 및 다중 어드바이저 테스트

--- src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
+++ src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
@@ -15,7 +15,7 @@ public class MultiAdvisorTest {
 
     @Test
     @DisplayName("여러 프록시")
-    void multiAdvisorTest() {
+    void multiAdvisorTest1() {
         // client -> proxy2(advisor2) -> proxy1(advisor1) -> target
 
         // 프록시 1 생성
@@ -32,6 +32,27 @@ public class MultiAdvisorTest {
         ServiceInterface proxy2 = (ServiceInterface) proxyFactory2.getProxy();
 
         proxy2.save();
+    }
+
+    @Test
+    @DisplayName("하나의 프록시, 여러 어드바이저")
+    void multiAdvisorTest2() {
+        // client -> proxy -> advisor2 -> advisor1 -> target
+
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+
+        // 프록시 1 생성
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+
+        proxyFactory.addAdvisor(advisor2);
+        proxyFactory.addAdvisor(advisor1);
+
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        // 실행
+        proxy.save();
     }
 
     @Slf4j
