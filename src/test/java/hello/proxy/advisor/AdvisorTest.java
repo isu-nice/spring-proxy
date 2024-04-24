dd975ccdafb07368909137eb6531d6f0adbc79bb
isu-nice <isu010614@naver.com>
2024-04-24 20:34:20 +0900

    test: 스프링이 제공하는 포인트컷 테스트

--- src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -11,9 +11,13 @@ import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 import java.lang.reflect.Method;
 
+/**
+ * Pointcut 활용 테스트: save()를 호출할 떄는 advice가 적용되고, find()를 호출할 떄는 적용되지 않도록 함
+ */
 public class AdvisorTest {
 
     @Test
@@ -36,6 +40,23 @@ public class AdvisorTest {
         ProxyFactory proxyFactory = new ProxyFactory(target);
 
         DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        proxy.save();
+        proxy.find();
+    }
+
+    @Test
+    @DisplayName("스프링이 제공하는 포인트컷")
+    void advisorTest3() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("save");
+
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, new TimeAdvice());
         proxyFactory.addAdvisor(advisor);
 
         ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
