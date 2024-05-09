977fc6ae0f87e354e76d7267eb11d6c1c952d1a5
isu-nice <isu010614@naver.com>
2024-05-09 18:51:40 +0900

    docs: 빈 후처리기 적용 설명 추가

--- src/main/java/hello/proxy/config/v4_postprocessor/postprocessor/PackageLogTracePostProcessor.java
+++ src/main/java/hello/proxy/config/v4_postprocessor/postprocessor/PackageLogTracePostProcessor.java
@@ -6,6 +6,15 @@ import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
+/*
+ * 적용 결과
+ * v1: JDK 동적 프록시 -> 인터페이스가 있으므로 JDK 동적 프록시가 적용됨
+ * v2, v3: CGLIB -> 구체 클래스만 있으므로 CGLIB 프록시 적용
+ *
+ * 중요!!
+ * 빈 후처리기 덕분에 v1, v2와 같이 수동으로 등록한 빈 뿐만 아니라
+ * 컴포넌트 스캔을 통해 등록한 v3 빈들도 프록시를 적용할 수 있다!!
+ */
 @Slf4j
 public class PackageLogTracePostProcessor implements BeanPostProcessor {
 
